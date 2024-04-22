d38910e939282708af5e8caf2236c6bf466d02ed
16Shadows <egor.0.00@mail.ru>
2024-04-22 14:58:40 +0500

    Configured proguard

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,9 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            signingConfig = signingConfigs.getByName("debug")
         }
     }
     compileOptions {
